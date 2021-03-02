a3be1856a78e0b7d740d4751b07987af161c9a51
Alan <seekincs@gmail.com>
2021-03-02 10:13:33 +0800

    v2

--- centerline_train_tools/centerline_train_tools.py
+++ centerline_train_tools/centerline_train_tools.py
@@ -1,40 +1,33 @@
-import torch
-from centerline_trainner import Trainer
-from data_provider_argu import DataGenerater
-from models.centerline_net import CenterlineNet
 import sys
 sys.path.append('..')
+from models.centerline_net import CenterlineNet
+from data_provider_argu import DataGenerater
+from centerline_trainner import Trainer
+import torch
 
-
-def get_dataset(save_num=0):
+def get_dataset(save_num = 0):
     '''
     :return: train set,val set
     '''
-    train_data_info_path = "../data_process_tools/patch_data/centerline_patch/train_save_d" + \
-        str(save_num)+"_train.csv"
+    train_data_info_path = "../data_process_tools/patch_data/centerline_patch/train_save_d"+str(save_num)+"_train.csv"
     train_pre_fix_path = "../data_process_tools/patch_data/"
     train_flag = 'train'
     train_transforms = None
     target_transform = None
-    train_dataset = DataGenerater(
-        train_data_info_path, train_pre_fix_path, 500, train_transforms, train_flag, target_transform)
+    train_dataset = DataGenerater(train_data_info_path, train_pre_fix_path, 500, train_transforms, train_flag, target_transform)
 
-    val_data_info_path = "../data_process_tools/patch_data/centerline_patch/train_save_d" + \
-        str(save_num)+"_val.csv"
+    val_data_info_path = "../data_process_tools/patch_data/centerline_patch/train_save_d"+str(save_num)+"_val.csv"
     val_pre_fix_path = "../data_process_tools/patch_data/"
     val_flag = 'val'
     test_valid_transforms = None
     target_transform = None
-    val_dataset = DataGenerater(val_data_info_path, val_pre_fix_path,
-                                500, test_valid_transforms, val_flag, target_transform)
+    val_dataset = DataGenerater(val_data_info_path, val_pre_fix_path, 500, test_valid_transforms, val_flag, target_transform)
 
     return train_dataset, val_dataset
-
 
 def cross_entropy(a, y):
     epsilon = 1e-9
     return torch.mean(torch.sum(-y * torch.log10(a + epsilon) - (1 - y) * torch.log10(1 - a + epsilon), dim=1))
-
 
 if __name__ == '__main__':
 
@@ -46,7 +39,7 @@ if __name__ == '__main__':
     curr_model_name = "centerline_net"
     # 球面上最多500个点，将所有的中心线方向进行分类，一共500个类
     max_points = 500
-    model = CenterlineNet(n_classes=max_points)
+    model = CenterlineNet(n_classes = max_points)
 
     batch_size = 64
     num_workers = 16
@@ -55,8 +48,7 @@ if __name__ == '__main__':
     # 初始化的学习率
     inital_lr = 0.001
 
-    optimizer = torch.optim.Adam(filter(
-        lambda p: p.requires_grad, model.parameters()), lr=inital_lr, weight_decay=0.001)
+    optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=inital_lr,weight_decay=0.001)
 
     trainer = Trainer(batch_size,
                       num_workers,
@@ -67,7 +59,7 @@ if __name__ == '__main__':
                       optimizer,
                       criterion,
                       max_points,
-                      save_num=save_num,
+                      save_num = save_num,
                       start_epoch=0,
                       max_epoch=100,
                       initial_lr=inital_lr)
